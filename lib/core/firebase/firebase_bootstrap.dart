e7de3167b2282780d1df7a84e13d66686dd0afe3
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2026-06-21 16:47:57 +0000

    feat(firebase): integrate Firebase Crashlytics and configuration files

--- lib/core/firebase/firebase_bootstrap.dart
+++ lib/core/firebase/firebase_bootstrap.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/foundation.dart';
+import 'package:aedify/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
 class FirebaseBootstrap {
   Future<void> initialize() async {
-    await Firebase.initializeApp();
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
     FlutterError.onError = (errorDetails) {
       FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
     };
